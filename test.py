3110bf62a0d97849c3a3267a16d5c1828fa00ba9
laua5 <laua5@middleton.school.nz>
2024-04-07 12:51:25 +1200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-choice = ""
 # Existing Menu
 combos = {
     "Value":
@@ -9,13 +8,20 @@ combos = {
         {"Cheese Burger": 6.69, "Large Fries": 2, "Smoothie": 2},
 }
 
+
 def search():
     while True:
-        combo_names = input("Please search for a combo name: ")
-        for combo_name, combo_info in combos.items():
-            if combo_name == combo_names:
-                combo_items = input(f"Please search for a combo item in"
-                                    f" {combo_names} combo: ")
+        combo_names = input("Please search for a combo name (x to exit): ")
+        if combo_names == "x":
+            break
+        else:
+            for combo_name, combo_info in combos.items():
+                if combo_name == combo_names:
+                    print(f"Here is the {combo_names} combo info:")
+                    print(f"{combos[f'{combo_names}']}")
+                else:
+                    print("Combo not found.")
+
 
 def add():
     while True:
@@ -29,16 +35,35 @@ def add():
                 combos[the_name][f'{first_item}'] = first_item
                 first_item_price = input(f"Please enter price of "
                                          f"{first_item}: ")
+                combos[the_name][f'{first_item}'] = first_item_price
                 add_more_item = input("Please enter another item to add "
                                       "(x to exit): ")
                 if add_more_item == "x":
                     break
                 else:
-                    combos[the_name][f'{add_more_item}'] = add_more_item
+                    while True:
+                        combos[the_name][f'{add_more_item}'] = add_more_item
+                        add_item_price = input(f"Please enter price of "
+                                               f"{add_more_item}: ")
+                        combos[the_name][f'{add_more_item}'] = add_item_price
+                        add_more_item = input("Please enter another item to"
+                                              " add (x to exit): ")
+                        if add_more_item == "x":
+                            break
+                break
+        break
 
 
+def print_list():
+    print("Here is the list of combos: ")
+    for combo_item, combo_info in combos.items():
+        print(f"\nCombo Name: {combo_item}")
+        for key in combo_info:
+            print(f"{key}: ${combo_info[key]}")
 
 
+def delete():
+    print()
 
 
 # Number Checking function (based off lucky unicorn)
@@ -62,18 +87,21 @@ def num_check(question, low, high):
         except ValueError:
             print(error)
 
-while choice != "3":
-    print("######### Welcome! ##########\n")
-    print("\nWhat would you like to do?\n1: Search contacts list\n2: Print the"
-          " full list\n3: Exit the system\n")
+
+print("######### Welcome! ##########")
+while True:
+    print("\nWhat would you like to do?\n1: Search combos list\n2: Print the"
+          " full list\n3: Add combo\n4: Delete Combo\n5: Exit")
     choice = num_check("Please enter your choice(number between 1 and 5): ",
                        1, 5)
     if choice == 1:
         search()
     elif choice == 2:
-        print_contacts()
+        print_list()
     elif choice == 3:
         add()
+    elif choice == 4:
+        delete()
     else:
         print("goodbye")
-
+        break
